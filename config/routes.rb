66168dcc9ef0eba92f8df501bfeccc15c1a28951
ericfransen <fransenep@gmail.com>
2014-10-02 12:41:56 -0600

    rips out devise, lots of local commits lost

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   root 'pages#home'
 
+  get "home" => "pages#home"
   get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
