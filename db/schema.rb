26565b8e7fabe387cff1f24da53d34fc9ed07828
ericfransen <fransenep@gmail.com>
2014-10-07 10:44:16 -0600

    add latitude and longitude to BCycleStations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007154512) do
+ActiveRecord::Schema.define(version: 20141007164007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20141007154512) do
     t.string   "zip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: true do |t|
