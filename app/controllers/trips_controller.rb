0388088a5b1e89e5589d0edd65b1b2ce0a8fdfa9
ericfransen <fransenep@gmail.com>
2014-10-15 20:57:47 -0600

    user can create trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,18 +1,20 @@
 class TripsController < ApplicationController
 
+  before_action :require_login
+
   def new
     @trip = Trip.new
   end
 
   def create
-    start_location = Location.create(latitude:  params[:starting_location][:latitude],
+    start_location = Location.create(latitude: params[:starting_location][:latitude],
                                      longitude: params[:starting_location][:longitude])
 
-    dest_location  = Location.create( address:  params[:destination_location][:address])
+    dest_location  = Location.create(address: params[:destination_location][:address])
 
-    trip = Trip.create(starting_location: start_location,
-                        destination_location: dest_location)
-                        # user: current_user)
+    trip = Trip.create(starting_location:    start_location,
+                       destination_location: dest_location,
+                       user:                 current_user)
 
     if trip.save
       redirect_to '/index'
@@ -20,4 +22,14 @@ class TripsController < ApplicationController
       render :new, notice: "Destination invalid."
     end
   end
+
+private
+
+   def require_login
+     unless current_user
+       flash[:error] = "You must log in with UBER to build a trip"
+       redirect_to root_path
+     end
+   end
+
 end
---
+++ spec/features/user_can_create_trip_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "User creating trip", type: :feature do
+  # before do
+  #   request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:uber]
+  # end
+
+  describe "A user" do
+
+    it "can log in and can create trip" do
+      VCR.use_cassette('create_trip_get_lat_longs') do
+        visit root_path
+        mock_auth_hash
+        click_link "Log in with UBER"
+        click_link "Use My Location"
+        fill_in 'enter your destination', :with => '1510 Blake St Denver CO'
+        click_button "Go"
+        save_and_open_page
+        expect(page).to have_content('Options')
+      end
+    end
+  end
+end
