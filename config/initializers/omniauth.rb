ac00e599f8ab2c05bf8cde33b21790f9ba0bf282
ericfransen <fransenep@gmail.com>
2014-11-19 10:41:09 -0700

    config full_host initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :uber, ENV['pusher_app_id'], ENV['pusher_secret']
 end
+
+if Rails.env.production?
+  OmniAuth.config.full_host = "https://localmotion.me"
+end
